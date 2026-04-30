ff97d57ab1614c8285a08cf3e24117bc5a4f69c0
codestackdev-ai <codestackdev.ai@gmail.com>
2026-05-01 02:15:40 +0700

    Add editable site contact config

---
+++ site-config.js
@@ -0,0 +1,7 @@
+const SITE_CONFIG = {
+  storeName: 'CORE RAMPAGE',
+  whatsappDisplay: '08161113164',
+  whatsappNumber: '628161113164',
+  telegram: '@codestackdev',
+  wechat: '@codestackdev'
+};
